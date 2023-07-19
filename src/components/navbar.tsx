6b4deac2004efd387470574e725956889838154a
Akash Yadav <akash2@gmail.com>
2023-07-19 17:08:26 +0530

    CSS save

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,16 @@
 
-import {Link} from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom';
+
 const Navbar = () => {
+    const [searchParams] = useSearchParams();
+    const todos̥Data = searchParams.get("todos") ||null;
+    
   return (
-    <nav>
-        <Link to="/" className=''>All</Link>
-        <Link to="/?todos=active" className=''>Active</Link>
-        <Link to="/?todos=completed" className=''>Completed</Link>
-    </nav>
+   <nav>
+    <Link to="/" className={todos̥Data === null ? "active" : ""} > All </Link>
+    <Link to="/?todos=active" className={todos̥Data === "active" ? "active" : ""} > Active </Link>
+    <Link to="/?todos=completed" className={todos̥Data === "completed" ? "active" :""} > Completed </Link>
+   </nav>
   )
 }
 
