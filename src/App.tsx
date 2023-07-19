3447afea217372b444e23fe5c39d657577f7bdf3
Akash Yadav <akash2@gmail.com>
2023-07-19 17:17:49 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import AddToDo from './components/addtodo'
 import Todos from './components/todos'
 import Navbar from './components/navbar'
+import "./App.css"
 const App = () => {
   return (
    <main>
